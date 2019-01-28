10340595a483a9a50548fcf928b238cb42424801
Alexander Lyabah <a.lyabah@checkio.org>
2019-01-28 15:19:35 +0200

    first ru version

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -8,8 +8,8 @@ requirejs(['ext_editor_io', 'jquery_190'],
         var io = new extIO({
             multipleArguments: true,
             functions: {
-                python: 'sum_two',
-                js: 'sumTwo'
+                python: 'useless_flight',
+                js: 'uselessFlight'
             }
         });
         io.start();
